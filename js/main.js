99a7190d0c0016bb26e9cc2eda3e681076898424
rasmith <robee_98_99@yahoo.com>
2024-05-23 18:22:00 -0400

    many many more changes plus clean up

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@
 const startButtonEl = document.getElementById('startButton'); //variable for my Start Game button
 const resetEl = document.getElementById('resetGame'); //reset game element
 const hangmanStatusEl = document.getElementById('hangman').children;  //grabs the Hangman element
-const message = ("Welcome to Hangman! Here is your Word: ");//Message to user
+let messageEl = document.querySelector('.message-header');
 const keyboardButtons = document.querySelectorAll(".keyboard button");
+const randomWordEl = document.getElementById('randomWord');
+const keyboardCont = document.querySelector('.keyboard');
 
 startButtonEl.addEventListener('click', handleStart);  //event listener for my Start Game button
 resetEl.addEventListener('click', resetGame);  //event listener for reseting game
-keyboardButtons.forEach(button => button.addEventListener('click', grabLetter)); //event listener for keyboard clicks
+keyboardButtons.forEach(button => button.addEventListener('click', grabLetter)); //using arrow function for event listener for keyboard clicks
 
 
 /**-------State----------- */
@@ -20,87 +22,87 @@ const maxGuessesAllowed = 7; //HANGMAN has 7 letters
 
 /**------------Random Word------------- */
 //Below is where the random word will be broken out into Array
-let arr = [];
-const randomWord = ("Sunflower");
-arr += randomWord.replace(',' , '');
-//arr = randomWord.split('').replace(',' , '');
+const randomWord = "AH";
 
 /*--------------Logic---------- */
-init ();
+init();
 
+function init() {
+    document.querySelector('.message-header').innerHTML = "Welcome to HANGMAN game!"; //Displays Message when coming into game
+    updateHangmanIndicator();
+    displayKeyboard(false);
+}
 
-function init () {
-    grabLetter();
-    
-   };
-
+function renderMessage() {  //displays message after clicking Start and showing randomword
+    messageEl.innerText = `Lets Begin! Below is your random word that contains ${randomWord.length} characters:`
+}
 
 //function to handle when user clicks Start Game
 function handleStart() {
     document.getElementById('startButton').classList.add("startClicked");
-    document.querySelector('.message-header').innerHTML = message;
-    showRandomWord();
+    // showRandomWord();
+    updateRandomWord();
+    renderMessage();
+    displayKeyboard(true);
 };
 
-//Once Start is clicked, the random word box will show
-function showRandomWord() {
-    document.getElementById('randomWord').classList.add("showWordBorder");
-    console.log(arr);
-    document.querySelector('.randomWord').innerHTML = arr;
-};
+
 
 //This function will handle the Hangman letters after each wrong guess
-function updateHangmanIndicator(){
-    //somehow update wrongGuesses to the count of bad guess
-    document.getElementById('hangman').getElementsByTagName('span')[wrongGuesses - 1].classList.add("wrong");
-    console.log(hangmanStatusEl);
+function updateHangmanIndicator() {
+    for (let i = 0; i < wrongGuesses; i++) {
+        hangmanStatusEl[i].style.color = 'red';
+    }
 
 };
 
 //this function will reset the game
-function resetGame(){   
+function resetGame() {
     //reset HangMan
     location.reload();  //this refreshes the browser
 
+
 };
+//keyboard letter click event handler
+function grabLetter(event) {  //grabs the event "click"
+    //disable the button clicked
+    event.target.disabled = true;  //this will disable button clicked
 
-function grabLetter(event) {
-   //disable the button clicked
-   for (let i = 0; i < keyboardButtons.length; i++) {
-   keyboardButtons[i].classList.add("changeButton")
-   }
-   //get letter from keyboardButtons
-   let letter = keyboardButtons;
-   console.log(letter);
-   if (randomWord.includes(letter)) {
-    //update the Random Word by highlighting the correct letter
-    //arr.forEach
-
-    //add letter to guessedLetters array
-
-    checkWin();
-   } else 
-    //update wrongGUesses
-    //update HangmanIndicator
-    //checkLoss function to see if lost
-    checkLoss();
-};
-
- function updateRandomWord (){
-    //replace underline with letter guessed correctly
-
- };
-
-function checkWin() {
-    //this will check to see if the randomWord has been guessed and if so
-    //present Message stating they won
-};
-function checkLoss() {
-    if (wrongGuesses !== maxGuessesAllowed){
-        //allow user to continue
-
-    }else {
-    //disable keyboard keys
-    //display message stating they have guessed the max (this is where Hangman should be fully shown)
+    if (randomWord.toUpperCase().includes(event.target.textContent)) {
+        guessedLetters.push(event.target.textContent);
+        updateRandomWord();
+        checkWin();
+    } else {
+        wrongGuesses++
+        updateHangmanIndicator();
+        checkLoss();
     }
 };
+
+function updateRandomWord() {
+    //replace underline with letter guessed correctly with ternary operator
+    randomWordEl.textContent = randomWord.split("").map(letter => (guessedLetters.includes(letter) ? letter : "_")).join(" ");   //mapping values from array to the value in textContent
+};
+
+function checkWin() {
+    if (randomWordEl.textContent.replace(/ /g, "") === randomWord) {   //used stackOverflow to figure out how to remove the spaces from the array to match the variable https://stackoverflow.com/questions/3794919/replace-all-spaces-in-a-string-with
+        messageEl.innerText = `You got it!`;
+        displayKeyboard(false);
+    };
+};
+function checkLoss() {
+    if (wrongGuesses >= maxGuessesAllowed) {
+        messageEl.innerText = `Go buy a dictionary`;
+        displayKeyboard(false);
+        //display message stating they have guessed the max (this is where Hangman should be fully shown)
+
+    }
+};
+
+function displayKeyboard(value) {
+    if (value === false) {
+        keyboardCont.style.display = 'none';
+    } else {
+        keyboardCont.style.display = 'block';
+    }
+}
